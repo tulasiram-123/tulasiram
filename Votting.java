1d44b2ef1c4d87dd60d3a32a6566ce7f1fa3fbc1
tulasiram-123 <tulasiram@suntechnoservices.com>
2020-02-27 11:38:52 -0800

    first

---
+++ Votting.java
@@ -0,0 +1,13 @@
+package com.suntechno.techno;
+
+public class Votting {
+	  public static void main(String[] args) {
+	   int age=24;
+	  if (age > 18) {
+	      System.out.println("elgible for votting"); 
+	    } 
+	    if (age < 18) {
+	      System.out.println("not elgible for votting"); 
+	    }  
+	}
+	}
