8accbe554f0f6ae7d2ef588f520352d192c1ae91
abakasrikanth <srikanth@suntechnoservices>
2020-03-03 14:24:32 +0530

    wjejdej

---
+++ src/main/java/com/example/tulasi/TulasiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tulasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulasiApplication.class, args);
+	}
+
+}
